7877a7abda9a07205f8fdf5dc4e6d0c26e3f5d38
Bahrami <m.m.bhrm85@gmail.com>
2026-04-08 16:26:37 +0330

    Add to notes.

--- api/.dev/InDevelopment/Note.cs
+++ api/.dev/InDevelopment/Note.cs
@@ -3,4 +3,6 @@
 public record Note : Entity {
     public required string Title   { get; init; }
     public required string Content { get; init; }
+    public required DateTimeOffset CreatedAtUtc { get; init; }
+    public required DateTimeOffset? UpdatedAtUtc { get; init; }
 }
