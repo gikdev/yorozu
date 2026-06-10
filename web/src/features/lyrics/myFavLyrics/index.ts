004381c523fc4bc8b2843115d94e19633fec93aa
Mohammad Mahdi Bahrami <bahrami@mailfa.com>
2026-06-10 22:27:48 +0330

    Add 4 more musics!

--- web/src/features/lyrics/myFavLyrics/index.ts
+++ web/src/features/lyrics/myFavLyrics/index.ts
@@ -1,6 +1,18 @@
 import type { LyricSong } from "../LyricSong"
 import { awa } from "./awa"
+import { futsuu } from "./futsuu"
 import { killShot } from "./killShot"
+import { kyoriKan } from "./kyoriKan"
 import { shiawase } from "./shiawase"
+import { souvenir } from "./souvenir"
+import { vitaPhilosophica } from "./vitaPhilosophica"
 
-export const myFavLyricSongs: LyricSong[] = [shiawase, awa, killShot]
+export const myFavLyricSongs: LyricSong[] = [
+  shiawase,
+  awa,
+  killShot,
+  kyoriKan,
+  souvenir,
+  vitaPhilosophica,
+  futsuu,
+]
---
+++ web/src/features/lyrics/myFavLyrics/kyoriKan.ts
@@ -0,0 +1,283 @@
+import type { LyricSong } from "../LyricSong"
+
+export const kyoriKan: LyricSong = {
+  id: "kyori-kan",
+  title: "Kyori-kan",
+  artist: "Hakoniwa Lily",
+  animeTitle: "Aharen-san wa Hakarenai",
+  animeUrl: "https://animegate.ir/anime/aharen-san-wa-hakarenai-13933",
+  audioUrl: "/lyrics/Kyori-kan.mp3",
+  defaultPrimaryLanguage: "ja",
+  lines: [
+    {
+      ja: "<ruby>大<rt>だい</rt></ruby><ruby>丈<rt>じょう</rt></ruby><ruby>夫<rt>ぶ</rt></ruby>　<ruby>大<rt>だい</rt></ruby><ruby>丈<rt>じょう</rt></ruby><ruby>夫<rt>ぶ</rt></ruby>",
+      romaji: "Daijoubu daijoubu",
+      en: "It's okay, It's okay",
+    },
+    {
+      ja: "いつも<ruby>通<rt>とお</rt></ruby>りでね",
+      romaji: "Itsumo touridene",
+      en: "Be as usual",
+    },
+    {
+      ja: "<ruby>内<rt>ない</rt></ruby><ruby>心<rt>しん</rt></ruby>も<ruby>距<rt>きょ</rt></ruby><ruby>離<rt>り</rt></ruby><ruby>感<rt>かん</rt></ruby>も<ruby>測<rt>はか</rt></ruby>れないけど",
+      romaji: "Naishin mo kyorikan mo hakarenaikedo",
+      en: "I can't measure my true feelings and the distance between us",
+    },
+    {
+      ja: "<ruby>側<rt>そば</rt></ruby>にいて　ずっといて　<ruby>毎<rt>まい</rt></ruby><ruby>日<rt>にち</rt></ruby><ruby>楽<rt>たの</rt></ruby>しもう",
+      romaji: "Soba ni ite zutto ite mainichi tanoshimou",
+      en: "But I'll be by your side for a long time and enjoy every day",
+    },
+    {
+      ja: "「<ruby>大<rt>たい</rt></ruby><ruby>切<rt>せつ</rt></ruby>」が<ruby>幸<rt>しあわ</rt></ruby>せでありますように",
+      romaji: "'taisetsu' ga shiawasedearimasu you ni",
+      en: "I hope my 'precious' is happy",
+    },
+    {
+      ja: "<ruby>続<rt>つづ</rt></ruby>きますように",
+      romaji: "Tsuzukimasu you ni",
+      en: "I hope it continues",
+    },
+    {
+      ja: "<ruby>個<rt>こ</rt></ruby><ruby>性<rt>せい</rt></ruby>を<ruby>大<rt>たい</rt></ruby><ruby>切<rt>せつ</rt></ruby>にしようとか<ruby>言<rt>い</rt></ruby>うのに",
+      romaji: "Kosei wo taisetsu ni shiyou toka iunoni",
+      en: "Even though people say we should cherish our individuality",
+    },
+    {
+      ja: "<ruby>胸<rt>むね</rt></ruby><ruby>張<rt>は</rt></ruby>って<ruby>生<rt>い</rt></ruby>きることもずかしいよね",
+      romaji: "Mune hatte ikiru koto muzukashiiyone",
+      en: "It's difficult to be proud of yourself",
+    },
+    {
+      ja: "<ruby>誰<rt>だれ</rt></ruby>かの<ruby>目<rt>め</rt></ruby><ruby>気<rt>き</rt></ruby>にしちゃったり<ruby>誤<rt>ご</rt></ruby><ruby>解<rt>かい</rt></ruby>したり",
+      romaji: "Dare ka no me ki ni shichatari gokai shi tari",
+      en: "Worry about what others think or misunderstand each other",
+    },
+    {
+      ja: "だから<ruby>君<rt>きみ</rt></ruby>の<ruby>理<rt>り</rt></ruby><ruby>解<rt>かい</rt></ruby><ruby>者<rt>しゃ</rt></ruby>なれたらいいな",
+      romaji: "Dakara kimi no rikaisha naretara iina",
+      en: "I wish to be your supporter",
+    },
+    {
+      ja: "くっついて　じっとして",
+      romaji: "Kuttsuite jitto shite",
+      en: "Be together, stay still",
+    },
+    {
+      ja: "<ruby>頼<rt>たよ</rt></ruby>ってほしくて",
+      romaji: "Tayotte hoshikute",
+      en: "I want you to rely on me",
+    },
+    {
+      ja: "ギュッとしてキュンとして",
+      romaji: "Gyut to shite kyun to shite",
+      en: "I want you to hug me, I want to get excited",
+    },
+    {
+      ja: "<ruby>頭<rt>あたま</rt></ruby><ruby>撫<rt>な</rt></ruby>でて",
+      romaji: "Atama nadete",
+      en: "Pet my head",
+    },
+    {
+      ja: "<ruby>優<rt>やさ</rt></ruby>しさ<ruby>包<rt>つつ</rt></ruby>まれ",
+      romaji: "Yasashi sa tsutsumare",
+      en: "I want to be enveloped by your kindness",
+    },
+    {
+      ja: "<ruby>大<rt>だい</rt></ruby><ruby>丈<rt>じょう</rt></ruby><ruby>夫<rt>ぶ</rt></ruby>　<ruby>大<rt>だい</rt></ruby><ruby>丈<rt>じょう</rt></ruby><ruby>夫<rt>ぶ</rt></ruby>　そのままがいいよ",
+      romaji: "Daijoubu daijoubu sonomama ga iiyo",
+      en: "It's okay, it's okay, stay as you are",
+    },
+    {
+      ja: "<ruby>内<rt>ない</rt></ruby><ruby>心<rt>しん</rt></ruby>も<ruby>距<rt>きょ</rt></ruby><ruby>離<rt>り</rt></ruby><ruby>感<rt>かん</rt></ruby>も<ruby>測<rt>はか</rt></ruby>れないけど",
+      romaji: "Naishin mo kyorikan mo hakarenaikedo",
+      en: "I can't measure my true feelings and the distance between us",
+    },
+    {
+      ja: "<ruby>側<rt>そば</rt></ruby>にいて　ずっといて　<ruby>毎<rt>まい</rt></ruby><ruby>日<rt>にち</rt></ruby><ruby>楽<rt>たの</rt></ruby>しもう",
+      romaji: "Soba ni ite zutto ite mainichi tanoshimou",
+      en: "But I'll be by your side for a long time and enjoy every day",
+    },
+    {
+      ja: "「<ruby>大<rt>たい</rt></ruby><ruby>切<rt>せつ</rt></ruby>」が<ruby>幸<rt>しあわ</rt></ruby>せでありますように",
+      romaji: "'taisetsu' ga shiawasedearimasu you ni",
+      en: "I hope my 'precious' is happy",
+    },
+    {
+      ja: "ありのままでいい",
+      romaji: "Arinomamade ii",
+      en: "It's okay to be as it is",
+    },
+    {
+      ja: "<ruby>不<rt>ぶ</rt></ruby><ruby>器<rt>き</rt></ruby><ruby>用<rt>よう</rt></ruby>でもいい",
+      romaji: "Bukiyou demo ii",
+      en: "It's okay to be clumsy",
+    },
+    {
+      ja: "<ruby>側<rt>そば</rt></ruby>でもっと<ruby>君<rt>きみ</rt></ruby>を<ruby>知<rt>し</rt></ruby>りたいんだ",
+      romaji: "Soba de motto kimi wo shiritai nda",
+      en: "I want to know you more by your side",
+    },
+    {
+      ja: "<ruby>付<rt>つ</rt></ruby>き<ruby>合<rt>あ</rt></ruby>ってるって<ruby>勘<rt>かん</rt></ruby><ruby>違<rt>ちが</rt></ruby>いされちゃったね",
+      romaji: "Tsukiatteru tte kanchigai sarechattane",
+      en: "People thought we are dating",
+    },
+    {
+      ja: "やだよね？ ごめんね　<ruby>私<rt>わたし</rt></ruby>なんかで",
+      romaji: "Yadayone? gomenne watashi nankade",
+      en: "Did it bother you? Sorry, it was me",
+    },
+    {
+      ja: "そう<ruby>見<rt>み</rt></ruby>えるんだと<ruby>思<rt>おも</rt></ruby>うと<ruby>恥<rt>は</rt></ruby>ずかしくて",
+      romaji: "Sou mieru nda to omouto hazukashikute",
+      en: "I was embarrassed by how people saw us",
+    },
+    {
+      ja: "<ruby>避<rt>さ</rt></ruby>けちゃった　<ruby>本当<rt>ほんとう</rt></ruby>は<ruby>嬉<rt>うれ</rt></ruby>しいのにね",
+      romaji: "Sakechatta hontouha ureshiinonine",
+      en: "That's why I avoided you, even though I was happy",
+    },
+    {
+      ja: "ほっといて　そっとして",
+      romaji: "Hottoite sotto shite",
+      en: "Leave me alone, don't worry about me",
+    },
+    {
+      ja: "もう<ruby>気<rt>き</rt></ruby>にしないで",
+      romaji: "Mou ki ni shinaide",
+      en: "Don't worry about me",
+    },
+    {
+      ja: "ギュッとされハッとした",
+      romaji: "Gyut to sare hat to shita",
+      en: "It made me realized when you hug me",
+    },
+    {
+      ja: "<ruby>隠<rt>かく</rt></ruby>したのに",
+      romaji: "Kakushitanoni",
+      en: "Even though I was hiding it",
+    },
+    {
+      ja: "<ruby>溢<rt>あふ</rt></ruby>れてしまいそう",
+      romaji: "Afureteshimai sou",
+      en: "It's about to overflow",
+    },
+    {
+      ja: "<ruby>変<rt>か</rt></ruby>わろうよ　<ruby>変<rt>か</rt></ruby>えようよ",
+      romaji: "Kawarouyo kaeyouyo",
+      en: "Let's change, I want to change",
+    },
+    {
+      ja: "このままじゃやだよ",
+      romaji: "Kono mama jayadayo",
+      en: "I don't want to stay this way",
+    },
+    {
+      ja: "<ruby>本<rt>ほん</rt></ruby><ruby>心<rt>しん</rt></ruby>も<ruby>感<rt>かん</rt></ruby><ruby>情<rt>じょう</rt></ruby>も<ruby>届<rt>とど</rt></ruby>けたいから",
+      romaji: "Honshin mo kanjou mo todoketaikara",
+      en: "I want to tell you my true feeling and all my emotions",
+    },
+    {
+      ja: "<ruby>側<rt>そば</rt></ruby>にいて　ずっといて　<ruby>毎<rt>まい</rt></ruby><ruby>日<rt>にち</rt></ruby>ありがとう",
+      romaji: "Soba ni ite zutto ite mainichi arigatou",
+      en: "Stay by me forever, thank you for everyday",
+    },
+    {
+      ja: "「<ruby>大<rt>たい</rt></ruby><ruby>切<rt>せつ</rt></ruby>」が<ruby>幸<rt>しあわ</rt></ruby>せでありますように",
+      romaji: "'taisetsu' ga shiawasedearimasu you ni",
+      en: "I hope my 'precious' is happy",
+    },
+    {
+      ja: "<ruby>笑<rt>わら</rt></ruby>っていてほしい",
+      romaji: "Waratteite hoshii",
+      en: "I want you to be smiling",
+    },
+    {
+      ja: "<ruby>見<rt>み</rt></ruby>まもってほしい",
+      romaji: "Mimamotte hoshii",
+      en: "I want you to watch me over",
+    },
+    {
+      ja: "あなたの<ruby>特<rt>とく</rt></ruby><ruby>別<rt>べつ</rt></ruby>になりたいんだ",
+      romaji: "Anata no tokubetsu ni naritai nda",
+      en: "I want to be your special",
+    },
+    {
+      ja: "<ruby>毎<rt>まい</rt></ruby><ruby>朝<rt>あさ</rt></ruby>が<ruby>楽<rt>たの</rt></ruby>しみで<ruby>早<rt>はや</rt></ruby>く<ruby>会<rt>あ</rt></ruby>いたくて",
+      romaji: "Maiasa ga tanoshimi de hayaku aitakute",
+      en: "I was looking forward to every morning, I wanted to see you as soon as possible",
+    },
+    {
+      ja: "<ruby>教<rt>きょう</rt></ruby><ruby>室<rt>しつ</rt></ruby>に<ruby>駆<rt>か</rt></ruby>け<ruby>足<rt>あし</rt></ruby>で　<ruby>探<rt>さが</rt></ruby>しあってる",
+      romaji: "Kyoushitsu ni kakeashi de sagashi atteru",
+      en: "We ran into a classroom and looked for each other",
+    },
+    {
+      ja: "いち<ruby>日<rt>にち</rt></ruby>ってこんなにも<ruby>短<rt>みじか</rt></ruby>いんだね",
+      romaji: "Ichinichi tte konnanimo mijikai ndane",
+      en: "I didn't realize how short a day is",
+    },
+    {
+      ja: "<ruby>魔<rt>ま</rt></ruby><ruby>法<rt>ほう</rt></ruby>みたいだ",
+      romaji: "Mahou mitaida",
+      en: "It's like magic",
+    },
+    {
+      ja: "<ruby>大<rt>だい</rt></ruby><ruby>丈<rt>じょう</rt></ruby><ruby>夫<rt>ぶ</rt></ruby>　<ruby>大<rt>だい</rt></ruby><ruby>丈<rt>じょう</rt></ruby><ruby>夫<rt>ぶ</rt></ruby>　いつも<ruby>通<rt>とお</rt></ruby>りでね",
+      romaji: "Daijoubu daijoubu itsumo touridene",
+      en: "It's okay, it's okay, be as usual",
+    },
+    {
+      ja: "ありのままの<ruby>君<rt>きみ</rt></ruby>を<ruby>好<rt>す</rt></ruby>きになったんだ",
+      romaji: "Arinomama no kimi wo suki ni nattanda",
+      en: "I fell in love with you as you are",
+    },
+    {
+      ja: "<ruby>側<rt>そば</rt></ruby>にいて　ずっといて　<ruby>毎<rt>まい</rt></ruby><ruby>日<rt>にち</rt></ruby><ruby>楽<rt>たの</rt></ruby>しもう",
+      romaji: "Soba ni ite zutto ite mainichi tanoshimou",
+      en: "Stay by me forever, let's enjoy everyday",
+    },
+    {
+      ja: "「<ruby>大<rt>たい</rt></ruby><ruby>切<rt>せつ</rt></ruby>」を<ruby>幸<rt>しあわ</rt></ruby>せにするよ",
+      romaji: "'taisetsu' wo shiawase ni suruyo",
+      en: "I'll treasure my 'precious'",
+    },
+    {
+      ja: "<ruby>大<rt>だい</rt></ruby><ruby>好<rt>す</rt></ruby>きで<ruby>大<rt>だい</rt></ruby><ruby>好<rt>す</rt></ruby>きで<ruby>仕<rt>し</rt></ruby><ruby>方<rt>かた</rt></ruby>ないんだよ",
+      romaji: "Daisukide daisukide shikatanai ndayoh",
+      en: "I love you, love you so much",
+    },
+    {
+      ja: "ヤキモチも<ruby>束<rt>そく</rt></ruby><ruby>縛<rt>ばく</rt></ruby>もしちゃうかもだよ？",
+      romaji: "Yakimochi mo sokubaku mo shichau kamodayo?",
+      en: "I may get jealous or be clingy",
+    },
+    {
+      ja: "<ruby>飽<rt>あ</rt></ruby>きないでずっといて　<ruby>毎<rt>まい</rt></ruby><ruby>日<rt>にち</rt></ruby><ruby>楽<rt>たの</rt></ruby>しもう",
+      romaji: "Akinaide zutto ite mainichi tanoshimou",
+      en: "Don't be sick of me and stay with me forever, let's enjoy everyday",
+    },
+    {
+      ja: "「<ruby>大<rt>たい</rt></ruby><ruby>切<rt>せつ</rt></ruby>」と<ruby>幸<rt>しあわ</rt></ruby>せになれますように",
+      romaji: "'taisetsu' to shiawase ni naremasu you ni",
+      en: "I wish to be happy with my 'precious'",
+    },
+    {
+      ja: "ありのままでいい",
+      romaji: "Arinomamade ii",
+      en: "It's okay to be as it is",
+    },
+    {
+      ja: "<ruby>不<rt>ぶ</rt></ruby><ruby>器<rt>き</rt></ruby><ruby>用<rt>よう</rt></ruby>でもいい",
+      romaji: "Bukiyou demo ii",
+      en: "It's okay to be clumsy",
+    },
+    {
+      ja: "<ruby>見<rt>み</rt></ruby>つめ<ruby>合<rt>あ</rt></ruby>って　<ruby>今<rt>いま</rt></ruby><ruby>二<rt>に</rt></ruby><ruby>人<rt>にん</rt></ruby>ゼロ<ruby>距<rt>きょ</rt></ruby><ruby>離<rt>り</rt></ruby>",
+      romaji: "Mitsumeatte ima ni nin zero kyori",
+      en: "Look at each others' eyes, right now, there's zero distance between us",
+    },
+  ],
+}
