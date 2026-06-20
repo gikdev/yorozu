1e19c208cf7dff28875b29a8b5743794ba9c97f3
Bahrami <m.m.bhrm85@gmail.com>
2026-06-20 11:48:43 +0330

    Add edit consumption track page.

--- web/src/routes/apps/hondana/tracks.$id.edit.tsx
+++ web/src/routes/apps/hondana/tracks.$id.edit.tsx
@@ -1,9 +1,32 @@
-import { createFileRoute } from "@tanstack/react-router"
+import { AppShell } from "#/common/molecules/AppShell"
+import {
+  PageHeader,
+  PageHeaderBackButton,
+} from "#/common/molecules/page-header"
+import { EditTrackSection } from "#/features/consumption-tracks/EditTrackSection"
+import { createFileRoute, linkOptions } from "@tanstack/react-router"
 
 export const Route = createFileRoute("/apps/hondana/tracks/$id/edit")({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/apps/hondana/tracks/$id/edit"!</div>
+  const { id } = Route.useParams()
+
+  return (
+    <AppShell>
+      <PageHeader
+        title="Edit Track"
+        left={
+          <PageHeaderBackButton
+            to={linkOptions({ to: "/apps/hondana/tracks" }).to}
+          />
+        }
+      />
+
+      <main className="flex-1 p-4 overflow-y-auto min-h-0 flex flex-col">
+        <EditTrackSection trackId={id} />
+      </main>
+    </AppShell>
+  )
 }
