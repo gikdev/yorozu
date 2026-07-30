9031c39e1466ac573a61f483dab66d24284e64a1
Bahrami <m.m.bhrm85@gmail.com>
2026-07-30 19:26:00 +0330

    Fix skeleton of other phrase player pages.

--- web/src/routes/apps/phrase-player/editor.tsx
+++ web/src/routes/apps/phrase-player/editor.tsx
@@ -1,9 +1,20 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, linkOptions } from '@tanstack/react-router'
+import { AppBar } from '#/common/molecules/page-header'
+
+const TITLE = 'Phrase Player - Editor'
 
 export const Route = createFileRoute('/apps/phrase-player/editor')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/apps/phrase-player/editor"!</div>
+  return (
+    <div className='h-dvh flex flex-col overflow-hidden bg-mist-950 text-mist-400 max-w-240 mx-auto w-full'>
+      <AppBar title={TITLE} parentPath={linkOptions({ to: '/apps/phrase-player' })} />
+
+      <main className='flex-1 flex flex-col gap-6 p-4 overflow-y-auto min-h-0'>
+        <p>WIP.</p>
+      </main>
+    </div>
+  )
 }
