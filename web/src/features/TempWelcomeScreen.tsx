0ccc8a646329387aed12d77f4ecff0a96d60c337
Bahrami <m.m.bhrm85@gmail.com>
2026-07-30 19:22:15 +0330

    Simplify welcome screen.

--- web/src/features/TempWelcomeScreen.tsx
+++ web/src/features/TempWelcomeScreen.tsx
@@ -4,12 +4,9 @@ export const TempWelcomeScreen = () => (
   <div className='font-main min-h-screen flex items-center justify-center bg-mist-950 px-4 py-8 text-mist-400'>
     <Link
       to='/apps'
-      className='text-mist-100 font-bold text-5xl hover:bg-mist-900 cursor-pointer p-8 rounded-xl group'
+      className='text-mist-100 font-bold text-5xl border-2 border-mist-900 hover:border-mist-800 cursor-pointer p-8 rounded-xl group flex flex-col items-center text-center transition-all hover:opacity-100 opacity-50'
     >
-      <span className='inline-block group-hover:hidden [writing-mode:vertical-rl]'>
-        お帰り、スペク。
-      </span>
-      <span className='hidden group-hover:inline-block'>Visit Apps</span>
+      お帰り、スペク
     </Link>
   </div>
 )
